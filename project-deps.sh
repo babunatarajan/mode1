251912d63ebb4287cbe01b6f7d55ed091b71d368
babunatarajan <babunatarajan6@gmail.com>
2016-05-31 15:37:48 +0530

    Create project-deps.sh

---
+++ project-deps.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+git clone 
+pip install -r pip-requirements.txt
+#django-evolution==0.6.7
+pip install https://github.com/beanbaginc/django-evolution/archive/release-0.6.7.zip
+
+#django-mailer==0.2a1.dev3
+pip install https://launchpad.net/ubuntu/+archive/primary/+files/django-mailer_0.2a1.dev3.orig.tar.gz

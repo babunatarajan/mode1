fe879fd8a0b8b6d1194e7d8679d0be64dc326dcf
Babu Natarajan <babunatarjan6@gmail.com>
2016-06-01 10:17:07 -0400

    bug fixing nginx and gunicorn scripts

--- generate-gunicorn-file.sh
+++ generate-gunicorn-file.sh
@@ -10,8 +10,7 @@ conf(){
 echo "Generating gunicorn file..."
 
 printf "
-
-description "Gunicorn application server handling $name.themode.net"
+description \"Gunicorn application server handling $name.themode.net\"
 
 start on runlevel [2345]
 stop on runlevel [!2345]
@@ -21,9 +20,9 @@ setuid www-data
 setgid www-data
 chdir /opt/$name
 
-env ACCESS_LOG="--access-logfile gunicorn/access.log"
-env ERROR_LOG="--error-logfile gunicorn/error.log"
-exec bin/gunicorn $ACCESS_LOG $ERROR_LOG  --workers 3 --bind unix:gunicorn/listen.sock E3.wsgi:application
+env ACCESS_LOG=\"--access-logfile gunicorn/access.log\"
+env ERROR_LOG=\"--error-logfile gunicorn/error.log\"
+exec bin/gunicorn \$ACCESS_LOG \$ERROR_LOG --workers 3 --bind unix:gunicorn/listen.sock E3.wsgi:application
 " > "/etc/init/gunicorn-$name.conf"
 
 echo "Successfully created the Gunicorn file..."
@@ -31,7 +30,6 @@ echo "You can manage Gunicorn service with following commands"
 echo "service gunicorn-$name start/stop/restart" 
 
 printf "
- 
 import os
 import sys
 
@@ -46,16 +44,17 @@ if not _parent(PROJECT_ROOT) in sys.path:
 if not PROJECT_ROOT in sys.path:
         sys.path.append(PROJECT_ROOT)
 
-path = /opt/$name/E3/
+path = '/opt/$name/E3/'
 if path not in sys.path:
-    sys.path.insert(0, /opt/$name/E3)
+    sys.path.insert(0, '/opt/$name/E3')
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'E3.production_settings'
 
 import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+application = django.core.handlers.wsgi.WSGIHandler() 
 " > /opt/$name/E3/wsgi.py
 
+sudo mkdir /opt/$name/gunicorn
 sudo chown -R www-data:www-data /opt/$name/
 }
 
@@ -63,30 +62,22 @@ echo -n "Please verify client name ($name)... is that correct [Yes/No]: "
 read clientname
 
 if [ $clientname == "Yes" ] || [ $clientname == "yes" ]; then
-   if [ ! -d /opt/$name/E3 ]; then
-      echo "Client directory does not exist please run setup.sh first"
+   if [ -d /opt/$name ] && [ ! -d /opt/$name/E3 ]; then
+      echo "Client directory exist but E3 folder not available. Please do SVN CO / Git Clone"
       exit
    else
-   conf
+      if [ -f "/etc/init/gunicorn-$name.conf" ]; then
+         echo -n "Gunicorn file already exist do you want to overwrite [Yes/No]: "
+         read owrite
+         if [ $owrite == "Yes" ] || [ $owrite == "yes" ]; then
+             conf
+         else
+             exit
+         fi
+      else
+         conf
+      fi
    fi
 else
    exit
 fi
-
-
-if  [ -f /etc/init/gunicorn-$name.conf ]; then
-  echo -n "Client already exist... You want to overwrite nginx file Yes/No"
-  read owrite
-  if [ $owrite == "Yes" ]; then
-      conf
-  else
-     exit
-  fi
-else
-  conf
-  exit 1
-fi
-
-
-
-
--- generate-nginx-file.sh
+++ generate-nginx-file.sh
@@ -1,51 +1,31 @@
 #!/bin/bash
 
-if [ -d /etc/nginx1 ]; then
+if [ ! -d /etc/nginx ]; then
    echo -n "Nginx not installed... Do you want to install Yes/No : "
    read nginxinstall
    if [ $nginxinstall  == "Yes" ]; then
+      sudo apt-get update
       sudo apt-get install nginx
    else
       exit
    fi
+fi
 
-echo -n "Please enter client and press [ENTER]: "
+echo -n "Please enter client name and press [ENTER]: "
 read name
 
-echo -n "Please verify client name ($name)... is that correct Yes/No :" 
-read clientname
-if [ $clientname == "Yes" ]; then
-   echo "Proceeding..."
-else
-   exit
-fi
-
-grep -i "$name" "/etc/nginx/sites-enabled/$name"
-
-if  [ $? == 0 ]; then
-  echo -n "Client already exist... You want to overwrite nginx file Yes/No"
-  read owrite
-  if [ $owrite == "Yes" ]; then
-      nginxconf
-  else
-     exit
-else
-  nginxconf
-  exit 1
-fi
 
 #Creating Nginx Conf File
 echo "Generating nginx conf file..."
-nginxconf(){
-   printf "
-server {
+conf(){
+   printf "server {
         listen       80;
         error_log /var/log/nginx/$name-error.log;
         access_log /var/log/nginx/$name-access.log;
 
         server_name  $name.themode.net ;
         ## Only allow these request methods ##
-        if ($request_method !~ ^(GET|HEAD|POST)$ ) {
+        if (\$request_method !~ ^(GET|HEAD|POST)/$ ) {
                 return 444;
         }
         ## Do not accept DELETE, SEARCH and other methods ##
@@ -75,18 +55,35 @@ server {
         # actual source files
         location / {
                 root /opt/$name/E3;
-                proxy_set_header    X-Real-IP   $remote_addr;
-                proxy_set_header    Host        $http_host;
-                proxy_set_header    X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header    X-Real-IP   \$remote_addr;
+                proxy_set_header    Host        \$http_host;
+                proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
                 proxy_pass          http://unix:/opt/$name/gunicorn/listen.sock;
         }
 }
 " > "/etc/nginx/sites-available/$name"
 
-echo "Creating soft link for Nginx Conf file $name ..."
-cd /etc/nginx/sites-enabled/
-ln -s ../sites-available/$name .
-cd ~
-echo "Successfully Nginx conf file has been generated"
-
+   echo "Creating soft link for Nginx Conf file $name ..."
+   cd /etc/nginx/sites-enabled/ && ln -s ../sites-available/$name .
+   cd ~
+   echo "Successfully Nginx conf file has been generated"
 }
+
+echo -n "Please verify client name ($name)... is that correct [Yes/No]: " 
+read clientname
+
+if [ $clientname == "Yes" ] || [ $clientname == "yes" ]; then
+   if [ -f /etc/nginx/sites-enabled/$name ]; then
+      echo -n "Client already exist... You want to overwrite nginx file [Yes/No]: "
+      read owrite
+      if [ $owrite == "Yes" ] || [ $owrite == "yes" ]; then
+         conf
+      else
+         exit
+      fi
+   else
+      conf
+   fi   
+else
+   exit
+fi
